df1706331b6e8ed17fd1833fc686a8289123fed8
Nguyen Minh Ngoc <minhngocnguyen.403@gmail.com>
2023-11-19 01:38:35 +0700

    :sparkles:

--- run.sh
+++ run.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+pip install -r requirement.txt
+
 python ./pre_train.py 
+
 
 zip_file="model.zip"
 wget --no-check-certificate "https://drive.usercontent.google.com/download?id=1Bq-809GB8--XFp330Ik3QcK2uVBxj4Ep&export=download&authuser=0&confirm=t&uuid=cd79fee8-cbdf-4815-b0c7-06815141e0e1&at=APZUnTXBBYWtwO6RUgnr4dD7yDkl:1700331759430" -O "$zip_file"
